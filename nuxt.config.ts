33c702b9e14fa2426b8dc8ab77e04968185539b1
liuzhe <liuzhe@qq.com>
2024-05-22 00:01:20 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,9 @@ export default defineNuxtConfig({
     devtools: {enabled: false},
     app: {
         head: {
-            link: [{rel: 'stylesheet', href: '/css/index.css'},{rel: 'stylesheet', href: ''}],
+            link: [{rel: 'stylesheet', href: '/css/index.css'},{rel: 'icon', type: "image/svg+xml", href: "/favicon.svg"}],
+            meta: [{name: "description", content: '关于前端,UI框架，设计，工具，AI，云平台，网络安全，其他'}],
+            title: "小哲的导航网站"
         }
-    }
+    },
 })
