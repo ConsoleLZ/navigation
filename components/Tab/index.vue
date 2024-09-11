31975d25aea98dba893b7492cc759529448c56ae
liuzhe <liuzhe@qq.com>
2024-09-11 22:24:25 +0800

    feat: add

--- components/Tab/index.vue
+++ components/Tab/index.vue
@@ -16,22 +16,6 @@
             </div>
         </div>
 
-        <input type="radio" name="github_hot" role="tab" class="tab" aria-label="css艺术" />
-        <div role="tabpanel" class="tab-content p-10">
-            <div class="box-flex">
-                <div v-for="item in data2" :key="item.id" class="card bg-base-100 shadow-xl">
-                    <div class="card-body">
-                        <h2 class="card-title">{{ item.name }}</h2>
-                        <p>{{ item.description }}</p>
-                        <div class="badge badge-primary badge-outline">{{ item.language }}</div>
-                        <div class="card-actions justify-end">
-                            <button class="btn btn-primary btn-sm" @click="onJumpPage(item.url)">Go</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <input type="radio" name="github_hot" role="tab" class="tab" aria-label="模板" />
         <div role="tabpanel" class="tab-content p-10">
             <div class="box-flex">
@@ -64,10 +48,42 @@
             </div>
         </div>
 
+        <input type="radio" name="github_hot" role="tab" class="tab" aria-label="AI" />
+        <div role="tabpanel" class="tab-content p-10">
+            <div class="box-flex">
+                <div v-for="item in data6" :key="item.id" class="card bg-base-100 shadow-xl">
+                    <div class="card-body">
+                        <h2 class="card-title">{{ item.name }}</h2>
+                        <p>{{ item.description }}</p>
+                        <div class="badge badge-primary badge-outline">{{ item.language }}</div>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary btn-sm" @click="onJumpPage(item.url)">Go</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <input type="radio" name="github_hot" role="tab" class="tab" aria-label="娱乐" />
         <div role="tabpanel" class="tab-content p-10">
             <div class="box-flex">
                 <div v-for="item in data5" :key="item.id" class="card bg-base-100 shadow-xl">
+                    <div class="card-body">
+                        <h2 class="card-title">{{ item.name }}</h2>
+                        <p>{{ item.description }}</p>
+                        <div class="badge badge-primary badge-outline">{{ item.language }}</div>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary btn-sm" @click="onJumpPage(item.url)">Go</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <input type="radio" name="github_hot" role="tab" class="tab" aria-label="其他" />
+        <div role="tabpanel" class="tab-content p-10">
+            <div class="box-flex">
+                <div v-for="item in data2" :key="item.id" class="card bg-base-100 shadow-xl">
                     <div class="card-body">
                         <h2 class="card-title">{{ item.name }}</h2>
                         <p>{{ item.description }}</p>
@@ -89,7 +105,7 @@ import type { GithubModel } from "~/types/github";
 
 // 网站开发
 const data1 = ref<GithubModel[]>()
-// css艺术
+// 其他
 const data2 = ref<GithubModel[]>()
 // 模板
 const data3 = ref<GithubModel[]>()
@@ -97,16 +113,19 @@ const data3 = ref<GithubModel[]>()
 const data4 = ref<GithubModel[]>()
 // 娱乐
 const data5 = ref<GithubModel[]>()
+// ai
+const data6 = ref<GithubModel[]>()
 const checked = ref(true)
 const onJumpPage = (url: string)=>{
     window.open(url)
 }
 fetch(`${baseURL}/json/github.json`).then(res => res.json()).then(res => {
     data1.value = res.exploitation
-    data2.value = res.css
+    data2.value = res.other
     data3.value = res.template
     data4.value = res.study
     data5.value = res.amusement
+    data6.value = res.ai
 })
 </script>
 
