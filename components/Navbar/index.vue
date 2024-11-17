193e1436b2608344327686a460d0cce80a41a548
liuzhe <liuzhe@qq.com>
2024-11-17 15:17:51 +0800

    style:修改顶部导航

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -1,23 +1,21 @@
 <script setup lang="ts">
-import { routerConfig } from '~/public/config'
-import { type roterModel } from '~/types/router'
-import { data as webData } from '~/public/data/webData';
-import { data as githubData } from '~/public/data/github';
-import { data as vscodeData } from '~/public/data/vscode';
-import { data as addonData } from '~/public/data/addon';
-import { data as h5GamesData } from '~/public/data/h5Games';
-import { data as projectData } from '~/public/data/project';
-import MiniSearch from 'minisearch'
+import { routerConfig } from "~/public/config";
+import { data as webData } from "~/public/data/webData";
+import { data as githubData } from "~/public/data/github";
+import { data as vscodeData } from "~/public/data/vscode";
+import { data as addonData } from "~/public/data/addon";
+import { data as h5GamesData } from "~/public/data/h5Games";
+import { data as projectData } from "~/public/data/project";
+import MiniSearch from "minisearch";
 
-
-const rConfig: roterModel[] = routerConfig
-const searchData = ref<any>([])
+const rConfig = routerConfig;
+const searchData = ref<any>([]);
 const changePage = (url: string) => {
-  location.href = url
-}
+  location.href = url;
+};
 const onJump = (url: string) => {
-  window.open(url)
-}
+  window.open(url);
+};
 // 防抖函数
 function debounce(func: any, delay: number) {
   let timerId: any;
@@ -32,90 +30,104 @@ function debounce(func: any, delay: number) {
   };
 }
 
-let webDocuments: any
-let githubDocuments: any
-let vscodeDocuments: any
-let addonDocuments: any
-let h5GamesDocuments: any
-let projectDocuments: any
+let webDocuments: any;
+let githubDocuments: any;
+let vscodeDocuments: any;
+let addonDocuments: any;
+let h5GamesDocuments: any;
+let projectDocuments: any;
 
 if (import.meta.client) {
-  let id: number = 1
+  let id: number = 1;
   switch (true) {
-    case location.href.includes('github'):
-      githubDocuments = Object.values(githubData).flat().map(item => {
-        return {
-          name: item.name,
-          description: item.description,
-          url: item.url,
-          id: id++
-        }
-      })
+    case location.href.includes("github"):
+      githubDocuments = Object.values(githubData)
+        .flat()
+        .map((item) => {
+          return {
+            name: item.name,
+            description: item.description,
+            url: item.url,
+            id: id++,
+          };
+        });
       break;
-    case location.href.includes('vscode'):
-      vscodeDocuments = Object.values(vscodeData).flat().map(item => {
-        return {
-          name: item.name,
-          description: item.des,
-          url: item.url,
-          id: id++
-        }
-      })
+    case location.href.includes("vscode"):
+      vscodeDocuments = Object.values(vscodeData)
+        .flat()
+        .map((item) => {
+          return {
+            name: item.name,
+            description: item.des,
+            url: item.url,
+            id: id++,
+          };
+        });
       break;
-    case location.href.includes('addon'):
-      addonDocuments = Object.values(addonData).flat().map(item => {
-        return {
-          name: item.name,
-          description: item.des,
-          url: item.url,
-          id: id++
-        }
-      })
+    case location.href.includes("addon"):
+      addonDocuments = Object.values(addonData)
+        .flat()
+        .map((item) => {
+          return {
+            name: item.name,
+            description: item.des,
+            url: item.url,
+            id: id++,
+          };
+        });
       break;
-    case location.href.includes('h5Games'):
-      h5GamesDocuments = Object.values(h5GamesData).flat().map(item => {
-        return {
-          name: item.name,
-          description: item.description,
-          url: item.url,
-          id: id++
-        }
-      })
+    case location.href.includes("h5Games"):
+      h5GamesDocuments = Object.values(h5GamesData)
+        .flat()
+        .map((item) => {
+          return {
+            name: item.name,
+            description: item.description,
+            url: item.url,
+            id: id++,
+          };
+        });
       break;
-    case location.href.includes('project'):
-      projectDocuments = Object.values(projectData).flat().map(item => {
-        return {
-          name: item.name,
-          description: item.description,
-          url: item.ckURL,
-          id: id++
-        }
-      })
+    case location.href.includes("project"):
+      projectDocuments = Object.values(projectData)
+        .flat()
+        .map((item) => {
+          return {
+            name: item.name,
+            description: item.description,
+            url: item.ckURL,
+            id: id++,
+          };
+        });
       break;
     default:
-      webDocuments = Object.values(webData).flat().map(item => {
-        return {
-          name: item.name,
-          description: item.description,
-          url: item.url,
-          id: id++
-        }
-      })
+      webDocuments = Object.values(webData)
+        .flat()
+        .map((item) => {
+          return {
+            name: item.name,
+            description: item.description,
+            url: item.url,
+            id: id++,
+          };
+        });
   }
 }
 
-const tokenizer = (str:any) => {
+const tokenizer = (str: any) => {
   // 分词逻辑，返回完整的单词以及单词的部分片段
   const words = str.match(/[\u4e00-\u9fa5]+|[a-zA-Z0-9]+/g) || [];
-  const subStrings:any = [];
+  const subStrings: any = [];
 
-  words.forEach((word:any) => {
-    if (/^[a-zA-Z0-9]+$/.test(word)) { // 英文或数字
+  words.forEach((word: any) => {
+    if (/^[a-zA-Z0-9]+$/.test(word)) {
+      // 英文或数字
       // 生成所有可能的子串
       for (let i = 1; i <= word.length; i++) {
         subStrings.push(...word.slice(0, i));
       }
-    } else { // 中文
+    } else {
+      // 中文
       // 生成所有可能的 n-gram 子串
       for (let i = 1; i <= word.length; i++) {
         for (let j = 0; j <= word.length - i; j++) {
@@ -125,37 +137,39 @@ const tokenizer = (str:any) => {
     }
   });
 
-  return subStrings.filter((value:any, index:any, self:any) => self.indexOf(value) === index); // 去重
+  return subStrings.filter(
+    (value: any, index: any, self: any) => self.indexOf(value) === index
+  ); // 去重
 };
 
 // 搜索
 const onSearch = debounce(function () {
-  searchData.value = []
-  const searchInput: any = document.getElementById('searchInput')
+  searchData.value = [];
+  const searchInput: any = document.getElementById("searchInput");
   let miniSearch = new MiniSearch({
-    fields: ['name', 'description'], // fields to index for full-text search
-    storeFields: ['name', 'description', 'url'], // fields to return with search results
-    tokenize: tokenizer
-  })
+    fields: ["name", "description"], // fields to index for full-text search
+    storeFields: ["name", "description", "url"], // fields to return with search results
+    tokenize: tokenizer,
+  });
 
   switch (true) {
-    case location.href.includes('github'):
+    case location.href.includes("github"):
       miniSearch.addAll(githubDocuments);
       break;
-    case location.href.includes('vscode'):
+    case location.href.includes("vscode"):
       miniSearch.addAll(vscodeDocuments);
       break;
-    case location.href.includes('addon'):
+    case location.href.includes("addon"):
       miniSearch.addAll(addonDocuments);
       break;
-    case location.href.includes('h5Games'):
+    case location.href.includes("h5Games"):
       miniSearch.addAll(h5GamesDocuments);
       break;
-    case location.href.includes('project'):
+    case location.href.includes("project"):
       miniSearch.addAll(projectDocuments);
       break;
     default:
-      miniSearch.addAll(webDocuments)
+      miniSearch.addAll(webDocuments);
   }
 
   miniSearch.autoSuggest(searchInput.value, {
@@ -163,50 +177,79 @@ const onSearch = debounce(function () {
       searchData.value.push({
         name: result.name,
         description: result.description,
-        url: result.url
-      })
-      return true
-    }
-  })
+        url: result.url,
+      });
+      return true;
+    },
+  });
 }, 500);
 
 // 搜索框失去焦点
 const onSearchBlur = (e: any) => {
   setTimeout(() => {
-    searchData.value = []
-  }, 150)
-}
+    searchData.value = [];
+  }, 150);
+};
 </script>
 
 <template>
   <div class="navbar bg-base-100">
     <img width="28px" alt="logo" src="/favicon.svg" />
     <div class="flex-1">
-      <a class="btn btn-ghost text-xl" @click="changePage('/')">小哲的导航网站</a>
+      <a class="btn btn-ghost text-xl" @click="changePage('/')"
+        >小哲的导航网站</a
+      >
     </div>
     <label class="input input-bordered flex items-center gap-2">
-      <input @focusout="onSearchBlur" @input="onSearch" id="searchInput" type="text" class="grow" placeholder="搜索" />
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="h-4 w-4 opacity-70">
-        <path fill-rule="evenodd"
+      <input
+        @focusout="onSearchBlur"
+        @input="onSearch"
+        id="searchInput"
+        type="text"
+        class="grow"
+        placeholder="搜索"
+      />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
+        fill="currentColor"
+        class="h-4 w-4 opacity-70"
+      >
+        <path
+          fill-rule="evenodd"
           d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-          clip-rule="evenodd" />
+          clip-rule="evenodd"
+        />
       </svg>
       <div class="alertSearch" v-show="searchData.length">
-        <div style="width: 100%;cursor: pointer;margin-bottom: 10px" v-for="item in searchData" :key="item.url"
-          @click="onJump(item.url)">
+        <div
+          style="width: 100%; cursor: pointer; margin-bottom: 10px"
+          v-for="item in searchData"
+          :key="item.url"
+          @click="onJump(item.url)"
+        >
           <div>{{ item.name }}</div>
           <div class="des">
             {{ item.description }}
           </div>
-          <hr>
+          <hr />
         </div>
       </div>
     </label>
-    <div class="nav">
-      <div class="nav-item" v-for="item in rConfig" :key="item.url" @click="changePage(item.url)">
-        <img width="18px" :src="item.icon">
-        <div>{{ item.name }}</div>
-      </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li v-for="item in rConfig" :key="item.name">
+          <a v-if="!item.children" @click="changePage(item.url)">{{ item.name }}</a>
+          <details v-else>
+            <summary>{{ item.name }}</summary>
+            <ul class="bg-base-100 rounded-t-none p-2">
+              <li v-for="childItem in item.children" :key="childItem.name">
+                <a @click="changePage(childItem.url)">{{ childItem.name }}</a>
+              </li>
+            </ul>
+          </details>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -217,31 +260,6 @@ const onSearchBlur = (e: any) => {
   position: sticky;
   top: 0;
   z-index: 9999;
-}
-
-.nav {
-  width: 805px;
-  overflow: auto;
-  margin-left: 20px;
-  padding: 10px;
-  min-width: 100px;
-}
-
-.nav-item {
-  display: flex;
-  gap: 5px;
-  margin-right: 40px;
-  cursor: pointer;
-}
-
-.nav-item>div {
-  transition: all 300ms;
-  white-space: nowrap;
-  font-size: 14px;
-}
-
-.nav-item>div:hover {
-  color: rgb(38, 29, 170);
 }
 
 .input {
@@ -258,8 +276,7 @@ const onSearchBlur = (e: any) => {
   max-height: 400px;
   overflow: auto;
   background-color: white;
-  box-shadow:
-    2.8px 2.8px 5.5px rgba(0, 0, 0, 0.02),
+  box-shadow: 2.8px 2.8px 5.5px rgba(0, 0, 0, 0.02),
     6.7px 6.7px 13.2px rgba(0, 0, 0, 0.028),
     12.5px 12.5px 24.8px rgba(0, 0, 0, 0.035),
     22.3px 22.3px 44.2px rgba(0, 0, 0, 0.042),
--- public/config.ts
+++ public/config.ts
@@ -1,44 +1,48 @@
-import {type roterModel} from '~/types/router'
-
-export const routerConfig:roterModel[] = [
-    {
-        url: '/',
-        name: '网站分享',
-        icon: '/images/navbar/share.svg'
-    },
-    {
-        url: '/github',
-        name: '优秀项目',
-        icon: '/images/navbar/github.svg'
-    },
-    {
-        url: '/software',
-        name: '软件下载',
-        icon: '/images/navbar/software.svg'
-    },
-    {
-        url: '/vscode',
-        name: 'vscode插件',
-        icon: '/images/navbar/vscode.svg'
-    },
-    {
-        url: '/addon',
-        name: '浏览器插件',
-        icon: '/images/navbar/addon.svg'
-    },
-    {
-        url: '/blender',
-        name: 'blender插件',
-        icon: '/images/navbar/blender.svg'
-    },
-    {
-        url: '/h5Games',
-        name: 'h5小游戏',
-        icon: '/images/navbar/games.svg'
-    },
-    {
-        url: '/project',
-        name: '我的项目',
-        icon: '/images/navbar/project.svg'
-    },
-]
+export const routerConfig = [
+  {
+    url: "/",
+    name: "网站分享",
+    icon: "/images/navbar/share.svg",
+  },
+  {
+    url: "/github",
+    name: "优秀项目",
+    icon: "/images/navbar/github.svg",
+  },
+  {
+    url: "/software",
+    name: "软件下载",
+    icon: "/images/navbar/software.svg",
+  },
+  {
+    name: "插件",
+    icon: "/images/navbar/addon.svg",
+    children: [
+      {
+        url: "/vscode",
+        name: "vscode插件",
+        icon: "/images/navbar/vscode.svg",
+      },
+      {
+        url: "/addon",
+        name: "浏览器插件",
+        icon: "/images/navbar/IE.svg",
+      },
+      {
+        url: "/blender",
+        name: "blender插件",
+        icon: "/images/navbar/blender.svg",
+      },
+    ],
+  },
+  {
+    url: "/h5Games",
+    name: "h5小游戏",
+    icon: "/images/navbar/games.svg",
+  },
+  {
+    url: "/project",
+    name: "我的项目",
+    icon: "/images/navbar/project.svg",
+  },
+];
